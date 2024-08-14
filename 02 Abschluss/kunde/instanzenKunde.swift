2863b98bf62d3e88c6ec544682e26137000fdcb6
Markus Wirtz <info@markuswirtz.net>
2024-08-14 16:14:06 +0200

    aktualisierung

--- 02 Abschluss/kunde/instanzenKunde.swift
+++ 02 Abschluss/kunde/instanzenKunde.swift
@@ -7,9 +7,9 @@
 
 import Foundation
 
-let kunde1 = Kunde(kundenNr: "K001", name: "Julia", kontostand: 2350.59, bonuspunkte: 21000, warenkorb: Warenkorb(produkte: [:]))
-let kunde2 = Kunde(kundenNr: "K002", name: "Gregor", kontostand: 3160.26, bonuspunkte: 14000, warenkorb: Warenkorb(produkte: [:]))
-let kunde3 = Kunde(kundenNr: "K003", name: "Peter", kontostand: 2110.94, bonuspunkte: 18000, warenkorb: Warenkorb(produkte: [:]))
+let kunde1 = Kunde(kundenNr: "K001", name: "Julia Hoffmann", kontostand: 2350.59, bonuspunkte: 21000, warenkorb: Warenkorb(produkte: [:]))
+let kunde2 = Kunde(kundenNr: "K002", name: "Gregor Schönberg", kontostand: 3160.26, bonuspunkte: 14000, warenkorb: Warenkorb(produkte: [:]))
+let kunde3 = Kunde(kundenNr: "K003", name: "Peter Kaufmann", kontostand: 2110.94, bonuspunkte: 18000, warenkorb: Warenkorb(produkte: [:]))
 
 let kundenListe = [
     kunde1,
--- 02 Abschluss/main.swift
+++ 02 Abschluss/main.swift
@@ -3,7 +3,4 @@ import Foundation
 var aktiverKunde = shop_1.kunden.randomElement()
 var shopStatus = ShopStatus.shopping
 
-print("Herzlich Willkommen in deinem AppleStore \n")
-print("Hallo \(aktiverKunde!.name), viel Spaß beim Shoppen.")
-
 shop_1.menueAnzeigen()
